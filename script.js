be7094b845f186118b3914e271966b3098f55f20
AbiPalani <87649223+AbiPalani@users.noreply.github.com>
2021-08-16 22:40:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+setTimeout(function(){
+    const container =document.querySelector("div");
+    const result = document.createElement("div");
+    result.className = "result";
+    result.innerText = "10";
+    container.append(result);
+    setTimeout(function(){
+        const container =document.querySelector("div");
+        const result = document.createElement("div");
+        result.className = "result";
+        result.innerText = "9";
+        container.append(result);
+        setTimeout(function(){
+            const container =document.querySelector("div");
+            const result = document.createElement("div");
+            result.className = "result";
+            result.innerText = "8";
+            container.append(result);
+            setTimeout(function(){
+                const container =document.querySelector("div");
+                const result = document.createElement("div");
+                result.className = "result";
+                result.innerText = "7";
+                container.append(result);
+                setTimeout(function(){
+                    const container =document.querySelector("div");
+                    const result = document.createElement("div");
+                    result.className = "result";
+                    result.innerText = "6";
+                    container.append(result);
+                    setTimeout(function(){
+                        const container =document.querySelector("div");
+                        const result = document.createElement("div");
+                        result.className = "result";
+                        result.innerText = "5";
+                        container.append(result);
+                        setTimeout(function(){
+                            const container =document.querySelector("div");
+                            const result = document.createElement("div");
+                            result.className = "result";
+                            result.innerText = "4";
+                            container.append(result);
+                            setTimeout(function(){
+                                const container =document.querySelector("div");
+                                const result = document.createElement("div");
+                                result.className = "result";
+                                result.innerText = "3";
+                                container.append(result);
+                                setTimeout(function(){
+                                    const container =document.querySelector("div");
+                                    const result = document.createElement("div");
+                                    result.className = "result";
+                                    result.innerText = "2";
+                                    container.append(result);
+                                    setTimeout(function(){
+                                        const container =document.querySelector("div");
+                                        const result = document.createElement("div");
+                                        result.className = "result";
+                                        result.innerText = "1";
+                                        container.append(result);
+                                        setTimeout(function(){
+                                            const container =document.querySelector("div");
+                                            const result = document.createElement("div");
+                                            result.className = "result";
+                                            result.innerText = "Happy New Year!!!";
+                                            container.append(result);
+                                        }, 2000)
+                                    }, 2000)
+                                }, 2000)
+                            }, 2000)
+                        }, 2000)
+                    }, 2000)
+                }, 2000)
+            }, 2000)
+        }, 2000)
+    }, 2000)
+}, 2000)
+
